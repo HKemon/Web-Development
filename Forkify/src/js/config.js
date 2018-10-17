d783f8ba57a3a748d01df3aa256225054f5a0935
emon <hk.emon.007@gmail.com>
2018-10-18 01:13:42 +0600

    Search MVC and Recipe MV complete

---
+++ Forkify/src/js/config.js
@@ -0,0 +1,2 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = '639da8d617288059be766947d9e35d09';
--- Forkify/src/js/index.js
+++ Forkify/src/js/index.js
@@ -1,4 +1,83 @@
-import string from './models/Search';
-import {add, mul} from "./views/searchView";
+import Search from './models/Search';
+import Recipe from './models/Recipe';
+import * as searchView from './views/searchView';
+import {elements, renderLoader, clearLoader} from './views/base';
 
-console.log(add(5, 7), string);
+const state = {
+    // search
+    // recipe
+};
+
+// Search Controller
+
+const controlSearch = async () => {
+    // Get query from view
+    const query = searchView.getInput();
+    // const query = 'pizza';
+
+    if (query) {
+        // New Search object and to the state
+        state.search = new Search(query);
+
+        // Prepare UI for Result
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        try {
+            // Search for recipes
+            await state.search.getResult();
+            // Render Result on UI
+            clearLoader();
+            searchView.renderResults(state.search.result, null);
+        } catch (e) {
+            alert("Error Searching : " + e);
+            clearLoader();
+        }
+    }
+};
+
+elements.searchField.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch()
+        .catch(e => {
+            alert(e);
+        });
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
+
+// Recipe Controller
+
+const controlRecipe = async () => {
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
+
+    if (id) {
+        state.recipe = new Recipe(id);
+
+        try {
+            await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
+
+            state.recipe.calcTime();
+            state.recipe.calServings();
+            console.log(state.recipe);
+        } catch (e) {
+            alert("Error Procession Recipe: " + e);
+        }
+
+    }
+};
+
+// window.addEventListener('hashchange',controlRecipe);
+// window.addEventListener('load',controlRecipe);
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
